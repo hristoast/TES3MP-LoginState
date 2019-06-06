afdd7e17a2e6be7836f5d3e12c07648b4f07dce9
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2019-06-05 21:11:00 -0500

    Update and prune the script
    * All methods related to the hacked ncgd plugin are gone.
    * Renamed the main file to 'main.lua'

--- README.md
+++ README.md
@@ -4,16 +4,12 @@ Set up a specific login state for players.
 
 ## Installation
 
-1. Place `LoginState.lua` into your `CoreScripts/scripts/custom/` directory.  Symlinks are OK.
+1. Place `LoginState.lua` into your `CoreScripts/scripts/custom/` directory.
 
 1. Add the following to `CoreScripts/scripts/customScripts.lua`:
 
-        require("custom/LoginState")
+        require("custom/LoginState/main")
 
 ## Caius Package
 
 Ensures players have the package for Caius when they log on, in case a quest reset was done.  Set `config.ensure_caius_package` to `false` if you don't want this.
-
-## NCGD Compatibility
-
-Lua to support properly initializing NCGD for TES3MP.  Set `LoginState.use_ncgd` to `false` if you don't want this.  Note that NCGD does not currently work, but when it does so too should this lua.
---
+++ main.lua
@@ -0,0 +1,35 @@
+local LoginState = {}
+
+
+local function dbg(msg)
+   --[[ Convenient logging wrapper. ]]--
+   tes3mp.LogMessage(enumerations.log.VERBOSE, "[ UniqueItems ]: " .. msg)
+end
+
+LoginState.ensureCaiusPkg = function(eventStatus, pid)
+   --[[
+      Ensure that the player has the package for Caius Cosades,
+      in the case that there was a quest reset and the player
+      already gave him the package.
+   --]]
+   if config.ensure_caius_package then
+      local caiuspkg = { refId = "bk_a1_1_caiuspackage", count = 1, charge = -1 }
+      local player = Players[pid]
+
+      if tableHelper.containsValue(WorldInstance.data.topics, "report to caius cosades") == false then
+         dbg("LoginState: Quest 'report to caius' has not been completed!")
+         local hasPkg = inventoryHelper.getItemIndex(player.data.inventory, "bk_a1_1_caiuspackage", -1)
+         if hasPkg then
+            dbg("LoginState: " .. player.accountName .. " has the package for Caius !")
+         else
+            dbg("LoginState: Ensuring the caius package in " .. player.accountName .."'s inventory !")
+            table.insert(player.data.inventory, caiuspkg)
+
+            player:LoadInventory()
+         end
+      end
+   end
+end
+
+-- Ensure the Caius package
+customEventHooks.registerHandler("OnPlayerAuthentified", LoginState.ensureCaiusPkg)
